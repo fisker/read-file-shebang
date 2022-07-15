02438eba0365b43560c5146cbe1c5b4f86bffe23
fisker Cheung <lionkay@gmail.com>
2022-07-15 18:24:17 +0800

    Remove crlfDelay

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import {createReadStream} from 'node:fs'
 import {createInterface} from 'node:readline'
 
-async function readShebang(file) {
-  const readline = createInterface({
-    input: createReadStream(file, {encoding: 'utf8', end: 149}),
-    crlfDelay: 0,
-  })
+const MAX_HASH_BANG_LENGTH = 150
+const FILE_STREAM_OPTIONS = {encoding: 'utf8', end: MAX_HASH_BANG_LENGTH - 1}
 
+async function readShebang(file) {
+  const stream = createReadStream(file, FILE_STREAM_OPTIONS)
+  const readline = createInterface({input: stream})
   const {value: firstLine} = await readline[Symbol.asyncIterator]().next()
 
   if (firstLine && firstLine.startsWith('#!')) {
