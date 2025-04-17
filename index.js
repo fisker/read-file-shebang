a5a159c13230cdbf24b54fe06efe33d44e9c7fea
fisker <lionkay@gmail.com>
2025-04-17 14:53:47 +0800

    Minor tweak

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const FILE_STREAM_OPTIONS = {encoding: 'utf8', end: MAX_HASH_BANG_LENGTH - 1}
 
 async function readShebang(file) {
   const stream = createReadStream(file, FILE_STREAM_OPTIONS)
-  const readline = createInterface({input: stream})
+  const readline = Object.assign(createInterface({input: stream}), {crlfDelay: 0})
   const {value: firstLine} = await readline[Symbol.asyncIterator]().next()
 
-  if (firstLine && firstLine.startsWith('#!')) {
+  if (firstLine?.startsWith('#!')) {
     return firstLine
   }
 }
